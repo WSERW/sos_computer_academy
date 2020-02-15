a9cfae515923f879a225684dddab1ba393354e1c
WSERW <44847286+WSERW@users.noreply.github.com>
2020-02-16 01:10:34 +0300

    v1.1

---
+++ sos_computer_academy/main_sos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainSosConfig(AppConfig):
+    name = 'main_sos'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\WSERW\PycharmProjects\sos_computer_academy\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
